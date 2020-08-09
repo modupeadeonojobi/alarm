f1e334fb1b21c0cdd182449b577b16f6f74c1941
Modupe Ade-Onojobi <66411673+modupeadeonojobi@users.noreply.github.com>
2020-08-09 07:22:40 +0100

    implemented alarm clock js
    Js of the alarm clock

--- index.js
+++ index.js
@@ -58,3 +58,63 @@ var countDwn = function(){
 }
 
 countDwn();
+
+//alarm clock.
+
+var sound = new Audio('landras.mp3');
+var timer;
+
+function setAlarm(el){
+    var time = document.getElementById('alarmTime').valueAsNumber;
+    if(isNaN(time)){
+    alert("Invalid DateTime");
+    return;
+}
+
+var alarm = new Date(time);
+var alarmTime = new Date(alarm.getUTCFullYear(),alarm.getUTCMonth(), alarm.getUTCDate(),alarm.getUTCHours(),alarm.getUTCMinutes(),alarm.getUTCSeconds());
+var duration = alarmTime.getTime() - (new Date()).getTime();
+
+if(duration <0){
+    alert('Time is already passed');
+    return;
+}
+
+timer = setTimeout(initAlarm, duration);
+    el.innerHTML = "<span class='glyphiconglyphicon-remove'></span> Cancel Alarm";
+    el.setAttribute('onclick','cancelAlarm(this);');
+    el.setAttribute('class','btnbtn-danger');
+}
+
+
+function cancelAlarm(el){
+    el.innerHTML = "<span class ='glyphiconglyphicon-time'></span> Set Alarm";
+    el.setAttribute('onclick','setAlarm(this);');
+    el.setAttribute('class','btnbtn-success');
+    clearTimeout(timer);
+}
+
+function initAlarm(){
+    sound.loop = true;
+    sound.play();
+    document.getElementById('alarmButton').style.display ='none';
+    document.getElementById('selectButton').style.display ='';
+}
+
+function stopAlarm(){
+    sound.pause();
+    sound.currentTime = 0;
+    document.getElementById('selectButton').style.display = 'none';
+    cancelAlarm(document.getElementById('alarmButton'));
+    document.getElementById('alarmButton').style.display = '';
+}
+
+function snoozeAlarm(){
+    stopAlarm();
+    setTimeout(() => {
+        initAlarm();
+    }, 300000);
+    button.innerText = "Cancel Alarm";
+    button.setAttribute('onclick','cancelAlarm(this);');
+}
+
